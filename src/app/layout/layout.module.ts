1538d88b9edda4348cfb415fdf162da35b0e82c5
pcchavez21 <l.camp980721@itses.edu.mx>
2020-10-18 14:38:20 -0500

    header navbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { LayoutRoutingModule } from './layout-routing.module';
 import {LayoutComponent} from "./layout.component";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import {LayoutComponent} from "./layout.component";
     LayoutComponent
   ],
   imports: [
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    SharedModule
   ]
 })
 export class LayoutModule { }
