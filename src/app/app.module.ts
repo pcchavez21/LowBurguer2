b8d8bce38b646af4846b9c8d99a58d343e590a8a
pcchavez21 <l.camp980721@itses.edu.mx>
2020-10-18 22:16:59 -0500

    background & navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
+import { AreaComponent } from './widgets/area/area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
 import {HeaderComponent} from "./components/header/header.component";
+import {HighchartsChartModule} from "highcharts-angular";
+import { AreaComponent } from './widgets/area/area.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    AreaComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    HighchartsChartModule
   ],
   exports:[
     HeaderComponent,
+
 
   ]
 })
